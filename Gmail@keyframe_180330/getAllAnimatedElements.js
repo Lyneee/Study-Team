8b7dec0483fd55ea5e9f6ce34683f69e93e03d15
erliu94 <erliu@merkleinc.com>
2018-03-30 18:32:56 +0800

    Revert "gmail"
    This reverts commit f59963eddc43e748244b3ac32d142a4f4a6c2b41.

--- Gmail@keyframe_180330/getAllAnimatedElements.js
+++
@@ -1,9 +0,0 @@
-var all = document.querySelectorAll('*');
-var allAnimated = new Array();
-for(var i=0; i < all.length; i++) {
-	var an = window.getComputedStyle(all[i], '').animationName;
-	if(an !== "none") {
-		allAnimated.push(all[i]);
-	}
-}
-console.log(allAnimated);
